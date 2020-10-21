17e5c4a323c7d49e7c02f2f7dc9605ce82f72d78
seungwon <alxndr@grovesoft.net>
2020-10-21 16:00:47 +0900

    Board Model 생성 & RestController -> Controller

--- src/main/java/com/example/board/IndexController.java
+++ src/main/java/com/example/board/IndexController.java
@@ -3,16 +3,18 @@ package com.example.board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Alexander
  * @date 2020-10-21
  **/
-@RestController
+@Controller
 @RequestMapping("")
 public class IndexController {
 
+    @ResponseBody
     @GetMapping("hello")
     public String hello() {
         return "Hello Stranger";
---
+++ src/main/java/com/example/board/domain/board/Board.java
@@ -0,0 +1,27 @@
+package com.example.board.domain.board;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+/**
+ * @author Alexander
+ * @date 2020-10-21
+ **/
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
